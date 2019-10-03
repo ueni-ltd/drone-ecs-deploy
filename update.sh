3e5a48d341b67835a08e5ebcdd694a40e9eddca2
dpinchukov-ueni <github-ueni@shapirus.net>
2019-10-03 15:35:48 +0300

    Add support for --max-definitions

--- update.sh
+++ update.sh
@@ -31,6 +31,10 @@ if [ -z ${PLUGIN_MIN} ]; then
   PLUGIN_MIN="100"
 fi
 
+if [ ! -z ${PLUGIN_MAX_DEFINITIONS} ]; then
+  MAX_DEFINITIONS="--max-definitions $PLUGIN_MAX_DEFINITIONS"
+fi
+
 if [ ! -z ${PLUGIN_AWS_ACCESS_KEY_ID} ]; then
   AWS_ACCESS_KEY_ID=$PLUGIN_AWS_ACCESS_KEY_ID
 fi
@@ -39,4 +43,4 @@ if [ ! -z ${PLUGIN_AWS_SECRET_ACCESS_KEY} ]; then
   AWS_SECRET_ACCESS_KEY=$PLUGIN_AWS_SECRET_ACCESS_KEY
 fi
 
-ecs-deploy --region ${PLUGIN_AWS_REGION} --cluster ${PLUGIN_CLUSTER} --image ${PLUGIN_IMAGE_NAME} --service-name ${PLUGIN_SERVICE} --timeout ${PLUGIN_TIMEOUT} --min ${PLUGIN_MIN} --max ${PLUGIN_MAX}
+ecs-deploy --region ${PLUGIN_AWS_REGION} --cluster ${PLUGIN_CLUSTER} --image ${PLUGIN_IMAGE_NAME} --service-name ${PLUGIN_SERVICE} --timeout ${PLUGIN_TIMEOUT} --min ${PLUGIN_MIN} --max ${PLUGIN_MAX} ${MAX_DEFINITIONS}
